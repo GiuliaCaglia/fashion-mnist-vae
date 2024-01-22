a0b67c4e59881a43b54c5ad9d6f5450c44511ce3
Giulia Caglia <cagliagiulia42@gmail.com>
2024-01-22 21:59:57 +0100

    Feature (autoencoder): Add Variational Autoencoder

--- src/fashion_mnist_vae/networks/autoencoder.py
+++ src/fashion_mnist_vae/networks/autoencoder.py
@@ -1,5 +1,8 @@
+from typing import List
+
 import pyro
 import pyro.distributions as dist
+import torch
 from torch import nn, optim
 
 from fashion_mnist_vae.networks import networks
@@ -40,14 +43,51 @@ class AutoEncoder(nn.Module):
 
 
 class VariationalAutoEncoder(nn.Module):
+    LATENT_SPACE = 256
+    THICKNESS = 16
+
     def __init__(self):
         super().__init__()
-        self.encoder = networks.Encoder()
-        self.decoder = networks.Decoder()
+        self.encoder = networks.Encoder(
+            latent_space=self.LATENT_SPACE, thickness=self.THICKNESS, return_std=True
+        )
+        self.decoder = networks.Decoder(
+            latent_space=self.LATENT_SPACE, thickness=self.THICKNESS
+        )
 
-    def guide(self, X):
+    def guide(self, x):
         pyro.module("encoder", self.encoder)
 
-        with pyro.plate("data", len(X)):
-            z_loc, z_scale = self.encoder(X)
+        with pyro.plate("data", len(x), subsample_size=len(x)):
+            z_loc, z_scale = self.encoder(x)
             pyro.sample("latent_space", dist.Normal(z_loc, z_scale).to_event(1))
+
+    def model(self, x):
+        pyro.module("decoder", self.decoder)
+        z_loc = torch.zeros(len(x), self.LATENT_SPACE).to(self.decoder.mainline[1].weight.device.type)
+        z_scale = torch.ones(len(x), self.LATENT_SPACE).to(self.decoder.mainline[1].weight.device.type)
+
+        with pyro.plate("data", len(x), subsample_size=len(x)):
+            z = pyro.sample("latent_space", dist.Normal(z_loc, z_scale).to_event(1))
+            loc_out = self.decoder(z)
+            pyro.sample("obs", dist.ContinuousBernoulli(probs=loc_out).to_event(3), obs=x)
+
+            return loc_out
+
+    def train(self, data_loader, epochs) -> List[float]:
+        pyro.clear_param_store()
+        model = self.model
+        guide = self.guide
+        criterion = pyro.infer.Trace_ELBO()
+        optim = pyro.optim.Adam({"lr": 5e-4})
+        svi = pyro.infer.SVI(model=model, guide=guide, loss=criterion, optim=optim)
+        losses = []
+        for epoch in range(epochs):
+            epoch_loss = 0
+            for batch in data_loader:
+                loss = svi.step(batch)
+                epoch_loss += loss / len(batch)
+            print(f"Epoch: {epoch+1}/{epochs}; Loss: {epoch_loss}")
+            losses.append(epoch_loss)
+
+        return losses
